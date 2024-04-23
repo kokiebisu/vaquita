c3bf2977793fcaf06912889b6d2c4ed2930fb5f7
kokiebisu <kokiebisu@icloud.com>
2024-04-22 19:20:13 -0600

    refactor server entrypoint code

--- app.rb
+++ app.rb
@@ -6,28 +6,18 @@ before do
   content_type :json
 end
 
-get '/' do
-  "Hello, World!"
-end
-
 post '/process' do
   data = JSON.parse(request.body.read)
   url = data['url']
-  command = data['command']
-  puts url, command
-  if command == 'url'
-    stdout, stderr, status = system("./process.sh #{url}")
-    if status.success?
-      stdout
-    else
-      "Error: #{stderr}"
-    end
-  elsif command == 'playlist'
-    stdout, stderr, status = system("./process_playlist.sh #{url}")
-    if status.success?
-      stdout
-    else
-      "Error: #{stderr}"
-    end
+  type = data['type']
+  puts url, type
+
+  script = type == 'url' ? "./process.sh" : "./process_playlist.sh"
+
+  stdout, stderr, status = Open3.capture3("#{script} #{url}")
+  if status.success?
+    stdout
+  else
+    "Error: #{stderr}"
   end
 end
