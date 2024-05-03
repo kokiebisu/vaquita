c3d44d0c7e607ef7482c9b6ec8679d389c343e25
kokiebisu <kokiebisu@icloud.com>
2024-05-03 08:30:27 -0600

    add logic to empty out the txt files

--- process_playlist.sh
+++ process_playlist.sh
@@ -16,6 +16,10 @@ RESOURCE_PATH=$(cat resource_path.txt)
 PLAYLIST_NAME_PATH=$(cat playlist_name_path.txt)
 PLAYLIST_COVER_IMG_URL_PATH=$(cat playlist_cover_img_url_path.txt)
 
+truncate -s 0 playlist_cover_img_url_path.txt
+truncate -s 0 playlist_name_path.txt
+truncate -s 0 resource_path.txt
+
 if [ -z "$RESOURCE_PATH" ]; then
     echo "Resource path is not specified"
     exit 1
@@ -34,5 +38,3 @@ echo -e "\033[0;36mRunning import_to_apple_music.sh...\033[0m"
 RESOURCE_PATH_ESCAPED=$(printf "%q" "$RESOURCE_PATH")
 
 echo "$PASSWORD" | sudo -S osascript music_script.applescript "$RESOURCE_PATH" "$PLAYLIST_NAME_PATH" "$PLAYLIST_COVER_IMG_URL_PATH"
-
-# rm -rf "$RESOURCE_PATH"
