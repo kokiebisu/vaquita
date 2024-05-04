ed7be94bfe88924c86e351cbcc8bcfd274aaec63
kokiebisu <kokiebisu@icloud.com>
2024-05-03 22:17:31 -0600

    update output_path of songs for playlist processing into 'songs' folder

--- lib/vaquita.rb
+++ lib/vaquita.rb
@@ -36,14 +36,13 @@ end
 
 def process_music_playlist(cookie_value, url, path)
   cover_img_url, playlist_name, urls = Tarsier.extract_playlist(cookie_value, url)
-  output_path = Pathname.new("#{path}/#{playlist_name}")
+  output_path = Pathname.new("#{path}/#{playlist_name}/songs")
   progressbar = ProgressBar.create(title: "Processing Song", total: urls.length, format: '%a |%b>>%i| %p%% %t')
   urls.each do |url|
     process_song(url, output_path, progressbar)
   end
   progressbar.finish unless progressbar.finished?
   cover_img_path = Pathname.new("#{path}/#{playlist_name}/playlist_cover.jpg")
-  # Download the cover image to the specified path
   Utils.download_image(cover_img_url, cover_img_path)
   {
     coverImgPath: cover_img_path.to_s,
